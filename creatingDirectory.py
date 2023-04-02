9e48a2c3fadfa506220100f527bd957d7c0ce53c
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-04-02 19:31:17 +0530

    Add files via upload

---
+++ creatingDirectory.py
@@ -0,0 +1,11 @@
+'''The OS module in Python provides functions for
+    creating and removing a directory (folder),
+    fetching its contents,
+    changing and identifying the current directory, etc'''
+import os
+
+
+path='//Users/kkd/Downloads/NEW_FOLDER'   # For windows the path might look like 'D:/'.
+
+
+os.mkdir(path)      # This mkdir() function will create a new folder(directory).
